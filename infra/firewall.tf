1bc8c21b6f4adce020bc07ebdf1a66b7b5595ac8
VoplestanPervert <yuggoth.spam@gmail.com>
2026-03-27 15:21:12 +0100

    updates

--- infra/firewall.tf
+++ infra/firewall.tf
@@ -2,7 +2,6 @@ resource "digitalocean_firewall" "app" {
   name        = "${local.prefix}-firewall"
   droplet_ids = [digitalocean_droplet.app.id]
 
-  # SSH — restrict ssh_allowed_ips to your own IP(s) in prod
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
@@ -16,21 +15,18 @@ resource "digitalocean_firewall" "app" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
-  # HTTP — for when you add a domain + SSL
   inbound_rule {
     protocol         = "tcp"
     port_range       = "80"
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
-  # HTTPS — for when you add SSL
   inbound_rule {
     protocol         = "tcp"
     port_range       = "443"
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
-  # Allow all outbound (package installs, Docker pulls, email, etc.)
   outbound_rule {
     protocol              = "tcp"
     port_range            = "all"
